9136ea0d015aaa66333006b5f4642ddd2c23e560
guifroes1984 <guilhermefroes26@gmail.com>
2024-08-01 23:48:24 -0300

    Criando os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { ProdutoComponent } from './components/produto/produto.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProdutoComponent,
+    ProdutosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
