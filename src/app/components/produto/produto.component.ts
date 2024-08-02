574dfa40789a98a863068dd07a5e527d946453cc
guifroes1984 <guilhermefroes26@gmail.com>
2024-08-02 00:26:50 -0300

    Carregando o array categorias no select

--- src/app/components/produto/produto.component.ts
+++ src/app/components/produto/produto.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/interfaces/Categoria';
 
 @Component({
   selector: 'app-produto',
   templateUrl: './produto.component.html',
   styleUrls: ['./produto.component.css']
 })
-export class ProdutoComponent {
+export class ProdutoComponent implements OnInit {
+
+  @Input()
+  categorias: Categoria [] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/interfaces/Categoria';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrls: ['./produtos.component.css']
 })
-export class ProdutosComponent {
+export class ProdutosComponent implements OnInit {
 
+  categorias: Categoria [] = [
+    {id: 1, nome: "Produção Própria"},
+    {id: 2, nome: "Nacional"},
+    {id: 3, nome: "Importado"},
+    {id: 4, nome: "Premium"},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
