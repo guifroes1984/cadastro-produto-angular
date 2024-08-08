d870cb8d780caaa13c073e8638b27623b0842c09
guifroes1984 <guilhermefroes26@gmail.com>
2024-08-08 01:17:12 -0300

    Criando rotas básicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'produto', component: ProdutosComponent },
+  { path: 'categorias', component: CategoriasComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
